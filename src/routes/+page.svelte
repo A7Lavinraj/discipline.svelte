2f80bce0ea25e0abe8d73dfdd76e02d3e132f9b2
A7Lavinraj <lavinrajmohan@gmail.com>
2025-01-01 17:14:34 +0530

    feat: add date converters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Hello from today event</h1>
+<script lang="ts">
+	import { convertDayInWord, convertMonthInWord } from '$lib/date';
+	const date = new Date();
+</script>
+
+<h2>{convertDayInWord(date.getDay())}</h2>
+<p>{date.getDate()}&nbsp;{convertMonthInWord(date.getMonth())}&nbsp;{date.getFullYear()}</p>
