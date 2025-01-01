23d283ad38dfa010b2a158e70114d6dc06a9d693
A7Lavinraj <lavinrajmohan@gmail.com>
2025-01-01 16:55:33 +0530

    build: add path alias for components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import Header from '../components/header.svelte';
+	import Header from '$components/header.svelte';
 	let { children } = $props();
 </script>
 
